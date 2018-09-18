f8675bcbef24cd8cd651e299d259f4112e963272
durgaduggirala <43164419+durgaduggirala@users.noreply.github.com>
2018-09-18 13:55:47 -0700

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+	public int add(int a, int b)
+		{
+		int c = a + b;
+		return c;
+	}
+	public int sub(int c, int d){
+		int e =d-c ;
+		return e;
+		
+	}
+	public int mul(int a1, int b1){
+		int c2 = a1 * b1 ;
+		return c2;
+	}
+	public int div(int c1, int d1){
+		int e1 = c1/d1;
+		return e1;
+		
+	}
+}
